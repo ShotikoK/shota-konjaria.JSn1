ffb9e31ed3378f85dcf52325fecef3b1b7b12a31
Shota Konjaria <westd898@gmail.com>
2024-10-30 02:07:54 +0400

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+let firstname = "Shota"
+let lastname = "Konjaria"
+let email = "javascript@gmail.com"
+let age = "20"
+let phonenumber = "+995577777888"
+let isAdult = age >= 18;
+
+console.log(firstname);
+console.log(lastname);
+console.log(email);
+console.log(age);
+console.log(phonenumber);
+console.log(isAdult);
+
+console.log("%c phonenumber", "color:red;");
+console.log("%c isAdult: " + isAdult, "color: yellow;");
